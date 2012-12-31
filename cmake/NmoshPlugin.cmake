86fc24d71324538f0ca051ee0ad9bff1703178e3
okuoku <mjt@cltn.org>
2012-12-31 15:37:41 +0900

    nmosh: Respect NMOSHPLUGIN_*_BUILD settings

--- cmake/NmoshPlugin.cmake
+++ cmake/NmoshPlugin.cmake
@@ -23,45 +23,47 @@ macro(do_add_nmosh_plugin nam)
     endif()
     option(NMOSHPLUGIN_${nam}_BUILD
         "Build nmosh plugin ${nam}" ${_default})
-    if(NMOSHPLUGIN_${nam}_EMBED)
-        add_definitions(-DNMOSHPLUGIN_EMBED)
-        set(_disposition STATIC)
-        set(_folder Libraries)
-        set(_dirs "")
-        set(_libs "")
-        list(APPEND _libs ${ZZNMOSHPLUGIN_ADDLIBS}
-            ${NMOSH_PLUGIN_LINK_LIBRARIES}
-            ${nam})
-        list(APPEND _dirs ${ZZNMOSHPLUGIN_ADDLIBDIR}
-            ${NMOSH_PLUGIN_LINK_DIRECTORIES})
-        set(ZZNMOSHPLUGIN_ADDLIBS ${_libs} 
-            CACHE STRING "nmosh internal" FORCE)
-        set(ZZNMOSHPLUGIN_ADDLIBDIR ${_dirs} 
-            CACHE STRING "nmosh internal" FORCE)
-    else()
-        set(_disposition MODULE)
-        set(_folder Plugins)
+    if(NMOSHPLUGIN_${nam}_BUILD)
+        if(NMOSHPLUGIN_${nam}_EMBED)
+            add_definitions(-DNMOSHPLUGIN_EMBED)
+            set(_disposition STATIC)
+            set(_folder Libraries)
+            set(_dirs "")
+            set(_libs "")
+            list(APPEND _libs ${ZZNMOSHPLUGIN_ADDLIBS}
+                ${NMOSH_PLUGIN_LINK_LIBRARIES}
+                ${nam})
+            list(APPEND _dirs ${ZZNMOSHPLUGIN_ADDLIBDIR}
+                ${NMOSH_PLUGIN_LINK_DIRECTORIES})
+            set(ZZNMOSHPLUGIN_ADDLIBS ${_libs} 
+                CACHE STRING "nmosh internal" FORCE)
+            set(ZZNMOSHPLUGIN_ADDLIBDIR ${_dirs} 
+                CACHE STRING "nmosh internal" FORCE)
+        else()
+            set(_disposition MODULE)
+            set(_folder Plugins)
+        endif()
+        link_directories(${NMOSH_PLUGIN_LINK_DIRECTORIES})
+        if(NMOSHPLUGIN_${nam}_EMBED)
+            add_library(${nam} ${_disposition} ${NMOSH_PLUGIN_C_SOURCES})
+        else()
+            set(_or ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+            set(_ol ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+            set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${NMOSH_UNINSTALLED_PATH})
+            set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${NMOSH_UNINSTALLED_PATH})
+            add_library(${nam} ${_disposition} 
+                ${NMOSH_PLUGIN_C_SOURCES})
+            set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${_or})
+            set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${_ol})
+            set_target_properties(${nam} PROPERTIES
+                FOLDER ${_folder}
+                PREFIX ""
+                SUFFIX ".mplg")
+            # Move library destination to the side of main executable
+            install(TARGETS ${nam} DESTINATION plugins)
+        endif()
+        target_link_libraries(${nam} ${NMOSH_PLUGIN_LINK_LIBRARIES})
     endif()
-    link_directories(${NMOSH_PLUGIN_LINK_DIRECTORIES})
-    if(NMOSHPLUGIN_${nam}_EMBED)
-        add_library(${nam} ${_disposition} ${NMOSH_PLUGIN_C_SOURCES})
-    else()
-        set(_or ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-        set(_ol ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
-        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${NMOSH_UNINSTALLED_PATH})
-        set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${NMOSH_UNINSTALLED_PATH})
-        add_library(${nam} ${_disposition} 
-            ${NMOSH_PLUGIN_C_SOURCES})
-        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${_or})
-        set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${_ol})
-        set_target_properties(${nam} PROPERTIES
-            FOLDER ${_folder}
-            PREFIX ""
-            SUFFIX ".mplg")
-        # Move library destination to the side of main executable
-        install(TARGETS ${nam} DESTINATION plugins)
-    endif()
-    target_link_libraries(${nam} ${NMOSH_PLUGIN_LINK_LIBRARIES})
 endmacro(do_add_nmosh_plugin)
 
 macro(add_nmosh_plugin nam)

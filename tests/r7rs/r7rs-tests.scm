e7ee951e370c5b91720a941d3a8cd2f6bf5445fe
okuoku <mjt@cltn.org>
2013-01-01 16:46:46 +0900

    r7rs-tests: Update

--- tests/r7rs/r7rs-tests.scm
+++ tests/r7rs/r7rs-tests.scm
@@ -253,6 +253,10 @@
 (let*-values (((root rem) (exact-integer-sqrt 32)))
   (test 35 (* root rem)))
 
+(let*-values (((root rem) (exact-integer-sqrt (expt 2 140))))
+  (test 0 rem)
+  (test (expt 2 140) (square root)))
+
 (test '(x y x y) (let ((a 'a) (b 'b) (x 'x) (y 'y))
   (let*-values (((a b) (values x y))
                 ((x y) (values a b)))
@@ -950,36 +954,36 @@
 (test #f (char? 'a))
 (test #f (char? 0))
 
-(test #t (char=? #\a #\a))
+(test #t (char=? #\a #\a #\a))
 (test #f (char=? #\a #\A))
-(test #t (char<? #\a #\b))
+(test #t (char<? #\a #\b #\c))
 (test #f (char<? #\a #\a))
 (test #f (char<? #\b #\a))
 (test #f (char>? #\a #\b))
 (test #f (char>? #\a #\a))
-(test #t (char>? #\b #\a))
-(test #t (char<=? #\a #\b))
+(test #t (char>? #\c #\b #\a))
+(test #t (char<=? #\a #\b #\b))
 (test #t (char<=? #\a #\a))
 (test #f (char<=? #\b #\a))
 (test #f (char>=? #\a #\b))
 (test #t (char>=? #\a #\a))
-(test #t (char>=? #\b #\a))
+(test #t (char>=? #\b #\b #\a))
 
 (test #t (char-ci=? #\a #\a))
-(test #t (char-ci=? #\a #\A))
+(test #t (char-ci=? #\a #\A #\a))
 (test #f (char-ci=? #\a #\b))
-(test #t (char-ci<? #\a #\B))
+(test #t (char-ci<? #\a #\B #\c))
 (test #f (char-ci<? #\A #\a))
 (test #f (char-ci<? #\b #\A))
 (test #f (char-ci>? #\A #\b))
 (test #f (char-ci>? #\a #\A))
-(test #t (char-ci>? #\B #\a))
-(test #t (char-ci<=? #\a #\B))
+(test #t (char-ci>? #\c #\B #\a))
+(test #t (char-ci<=? #\a #\B #\b))
 (test #t (char-ci<=? #\A #\a))
 (test #f (char-ci<=? #\b #\A))
 (test #f (char-ci>=? #\A #\b))
 (test #t (char-ci>=? #\a #\A))
-(test #t (char-ci>=? #\B #\a))
+(test #t (char-ci>=? #\b #\B #\a))
 
 (test #t (char-alphabetic? #\a))
 (test #f (char-alphabetic? #\space))
@@ -1060,32 +1064,32 @@
 (test "a-c" (let ((str (string #\a #\b #\c))) (string-set! str 1 #\-) str))
 
 (test #t (string=? "" ""))
-(test #t (string=? "abc" "abc"))
+(test #t (string=? "abc" "abc" "abc"))
 (test #f (string=? "" "abc"))
 (test #f (string=? "abc" "aBc"))
 
 (test #f (string<? "" ""))
 (test #f (string<? "abc" "abc"))
-(test #t (string<? "abc" "abcd"))
+(test #t (string<? "abc" "abcd" "acd"))
 (test #f (string<? "abcd" "abc"))
 (test #t (string<? "abc" "bbc"))
 
 (test #f (string>? "" ""))
 (test #f (string>? "abc" "abc"))
 (test #f (string>? "abc" "abcd"))
-(test #t (string>? "abcd" "abc"))
+(test #t (string>? "acd" "abcd" "abc"))
 (test #f (string>? "abc" "bbc"))
 
 (test #t (string<=? "" ""))
 (test #t (string<=? "abc" "abc"))
-(test #t (string<=? "abc" "abcd"))
+(test #t (string<=? "abc" "abcd" "abcd"))
 (test #f (string<=? "abcd" "abc"))
 (test #t (string<=? "abc" "bbc"))
 
 (test #t (string>=? "" ""))
 (test #t (string>=? "abc" "abc"))
 (test #f (string>=? "abc" "abcd"))
-(test #t (string>=? "abcd" "abc"))
+(test #t (string>=? "abcd" "abcd" "abc"))
 (test #f (string>=? "abc" "bbc"))
 
 (test #t (string-ci=? "" ""))
@@ -1110,7 +1114,7 @@
 (test #f (string-ci>=? "abc" "aBcD"))
 (test #t (string-ci>=? "ABCd" "aBc"))
 
-(test #t (string-ci=? "ΑΒΓ" "αβγ"))
+(test #t (string-ci=? "ΑΒΓ" "αβγ" "αβγ"))
 (test #f (string-ci<? "ΑΒΓ" "αβγ"))
 (test #f (string-ci>? "ΑΒΓ" "αβγ"))
 (test #t (string-ci<=? "ΑΒΓ" "αβγ"))
@@ -1630,6 +1634,10 @@
 (test #vu8(1 2 3) (read-bytevector 3 (open-input-bytevector #vu8(1 2 3))))
 (test #vu8(1 2 3) (read-bytevector 3 (open-input-bytevector #vu8(1 2 3 4))))
 
+(test #t
+    (let ((bv (bytevector 1 2 3 4 5)))
+      (eof-object? (read-bytevector! bv (open-input-bytevector #vu8())))))
+
 (test #vu8(6 7 8 9 10)
   (let ((bv (bytevector 1 2 3 4 5)))
     (read-bytevector! bv (open-input-bytevector #vu8(6 7 8 9 10)) 0 5)
@@ -1807,7 +1815,7 @@
 ;;
 ;;   (input-str expected-value (input-str))
 
-(test-numeric-syntaxes ;; FIXME: mosh
+(test-numeric-syntaxes
  ;; Simple
  ("1" 1)
  ("+1" 1 "1")
@@ -1889,7 +1897,8 @@
  ("-inf.0-inf.0i" (make-rectangular -inf.0 -inf.0) "-Inf.0-Inf.0i")
  ("+inf.0-inf.0i" (make-rectangular +inf.0 -inf.0) "+Inf.0-Inf.0i")
  ;; Complex numbers (polar notation)
- ("1@2" -0.416146836547142+0.909297426825682i "-0.416146836547142+0.909297426825682i")
+ ;; Need to account for imprecision in write output.
+ ;; ("1@2" -0.416146836547142+0.909297426825682i "-0.416146836547142+0.909297426825682i")
  ;; Base prefixes
  ("#x11" 17 "17")
  ("#X11" 17 "17")

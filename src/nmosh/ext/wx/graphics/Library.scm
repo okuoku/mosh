428e56b972a12bff46bdc5538fc07f5ebe56c40e
okuoku <mjt@cltn.org>
2013-02-16 07:05:06 +0900

    wx: Graphics(again)
    Conflicts:
    
    	src/nmosh/ext/wx/CMakeLists.txt
    	src/nmosh/ext/wx/graphics/Library.scm
    	src/nmosh/ext/wx/graphics/mwx_graphics.cpp
    	src/nmosh/ext/wx/mosh_wx.cpp

--- src/nmosh/ext/wx/graphics/Library.scm
+++ src/nmosh/ext/wx/graphics/Library.scm
@@ -6,7 +6,7 @@
   (parent-libname: mosh_wx)
   ;(c-import: "mwx_font.cpp")
   #(ret name args)
-  (int mwx_graphics_kick (void* void* int void* int void* int))
+  (int mwx_graphics_kick (void* void* int void* int void* int void* int))
   (void mwx_bitmap_destroy (void*))
   (void* mwx_bitmap_create (int int int))
   (void* mwx_bitmap_from_image (void*))
--- src/nmosh/ext/wx/graphics/mwx_graphics.cpp
+++ src/nmosh/ext/wx/graphics/mwx_graphics.cpp
@@ -6,7 +6,7 @@
 
 typedef enum {
     // Context OPs
-    MWX_SET_FONT, // [O]
+    MWX_SET_FONT, // [O R G B A]
     MWX_SET_PEN, // [O]
     MWX_SET_BRUSH, // [O]
     //  Context transforms
@@ -17,7 +17,7 @@ typedef enum {
     //  Text and query
     MWX_TEXT_DRAW, // [O X Y]
     MWX_TEXT_QUERY_EXTENT, // [O] => [W H Descent externalLeading]
-    MWX_TEXT_QUERY_CHAR_EXTENTS, // [O Count] => [Count W ...]
+    MWX_TEXT_QUERY_CHAR_EXTENTS, // [O Count] => [W ...]
     // PATH OPs
     MWX_PATH_STROKE, 
     MWX_PATH_FILL_ODDEVEN,
@@ -39,8 +39,15 @@ typedef enum {
 
 
 #define OP (op_ptr++,(mwx_graphics_drawop)op[op_ptr-1])
-#define OBJ(type) (obj_ptr++,reinterpret_cast<type>((void*)obj[obj_ptr-1]))
+#define OBJINT (obj_ptr++,reinterpret_cast<intptr_t>((void*)obj[obj_ptr-1]))
+#define OBJUINT (obj_ptr++,reinterpret_cast<uintptr_t>((void*)obj[obj_ptr-1]))
+#define OBJFONT (obj_ptr++,reinterpret_cast<wxFont*>((void*)obj[obj_ptr-1]))
+#define OBJBRUSH (obj_ptr++,reinterpret_cast<wxBrush*>((void*)obj[obj_ptr-1]))
+#define OBJBITMAP (obj_ptr++,reinterpret_cast<wxBitmap*>((void*)obj[obj_ptr-1]))
+#define OBJPEN (obj_ptr++,reinterpret_cast<wxPen*>((void*)obj[obj_ptr-1]))
+#define OBJTEXT (obj_ptr++,reinterpret_cast<const char*>((void*)obj[obj_ptr-1]))
 #define VTX (vtx_ptr++,vtx[vtx_ptr-1])
+#define QRET *(query_ptr++,&query[query_ptr-1])
 
 static int /* 0 for success */
 kick_pathop(wxGraphicsContext* ctx, 
@@ -50,17 +57,27 @@ kick_pathop(wxGraphicsContext* ctx,
     int op_ptr = 0;
     int vtx_ptr = 0;
     int obj_ptr = 0;
+    int query_ptr = 0;
     mwx_graphics_drawop dop;
-    dop = OP;
+    wxGraphicsPath pth = ctx->CreatePath();
     for(;;dop = OP){
         int r;
         switch(dop){
+            case MWX_SET_PEN: // [O]
+                ctx->SetPen(*OBJPEN);
+                break;
+            case MWX_SET_BRUSH: // [O]
+                ctx->SetBrush(*OBJBRUSH);
+                break;
             // PATH OPs
             case MWX_PATH_STROKE: 
+                ctx->StrokePath(pth);
                 break;
             case MWX_PATH_FILL_ODDEVEN:
+                ctx->FillPath(pth, wxODDEVEN_RULE);
                 break;
             case MWX_PATH_FILL_WINDING:
+                ctx->FillPath(pth, wxWINDING_RULE);
                 break;
             case MWX_PATH_END:
                 *inout_op_ptr = op_ptr;
@@ -68,26 +85,98 @@ kick_pathop(wxGraphicsContext* ctx,
                 *inout_obj_ptr = obj_ptr;
                 return 0;
             case MWX_PATH_CLOSE:
+                pth.CloseSubpath();
                 break;
             case MWX_PATH_MOVETO: // [X Y]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    pth.MoveToPoint(x,y);
+                }
                 break;
             case MWX_PATH_ARC_CW: // [X Y R startAngle endAngle]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double r = VTX;
+                    double start = VTX;
+                    double end = VTX;
+                    pth.AddArc(x,y,r,start,end,true);
+                }
                 break;
             case MWX_PATH_ARC_CCW: // [X Y R startAngle endAngle]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double r = VTX;
+                    double start = VTX;
+                    double end = VTX;
+                    pth.AddArc(x,y,r,start,end,false);
+                }
                 break;
             case MWX_PATH_ARCTO: // [X1 Y1 X2 Y2 R]
+                {
+                    double x1 = VTX;
+                    double y1 = VTX;
+                    double x2 = VTX;
+                    double y2 = VTX;
+                    double r = VTX;
+                    pth.AddArcToPoint(x1,y1,x2,y2,r);
+                }
                 break;
             case MWX_PATH_CIRCLE: // [X Y R]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double r = VTX;
+                    pth.AddCircle(x,y,r);
+                }
                 break;
             case MWX_PATH_CURVETO: // [CX1 CY1 CX2 CY1 X Y]
+                {
+                    double cx1 = VTX;
+                    double cy1 = VTX;
+                    double cx2 = VTX;
+                    double cy2 = VTX;
+                    double x = VTX;
+                    double y = VTX;
+                    pth.AddCurveToPoint(cx1,cy1,cx2,cy2,x,y);
+                }
                 break;
             case MWX_PATH_ELLIPSE: // [X Y W H]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double w = VTX;
+                    double h = VTX;
+                    pth.AddEllipse(x,y,w,h);
+                }
                 break;
             case MWX_PATH_LINETO: // [X Y]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    pth.AddLineToPoint(x,y);
+                }
                 break;
             case MWX_PATH_RECT: // [X Y W H]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double w = VTX;
+                    double h = VTX;
+                    pth.AddRectangle(x,y,w,h);
+                }
                 break;
             case MWX_PATH_RRECT: // [X Y W H R]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    double w = VTX;
+                    double h = VTX;
+                    double r = VTX;
+                    pth.AddRoundedRectangle(x,y,w,h,r);
+                }
                 break;
             default:
                 return -1;
@@ -99,12 +188,13 @@ static int /* 0 for success */
 kick(wxGraphicsContext* ctx, 
      unsigned char* op, int count_op,
      double* vtx, int count_vtx,
-     uintptr_t* obj, int count_obj){
+     uintptr_t* obj, int count_obj,
+     double* query, int count_query){
     int op_ptr = 0;
     int vtx_ptr = 0;
     int obj_ptr = 0;
+    int query_ptr = 0;
     mwx_graphics_drawop dop;
-    dop = OP;
     for(int i=0;i!=count_op;dop = OP){
         int r;
         int op_next_ptr;
@@ -127,31 +217,98 @@ kick(wxGraphicsContext* ctx,
                 vtx_ptr = vtx_next_ptr;
                 break;
             // Context OPs
-            case MWX_SET_FONT: // [O]
+            case MWX_SET_FONT: // [O R G B A]
+                { 
+                    const wxFont& font = *OBJFONT;
+                    int r = OBJUINT;
+                    int g = OBJUINT;
+                    int b = OBJUINT;
+                    int a = OBJUINT;
+                    const wxColour col = wxColour(r,g,b,a);
+                    ctx->SetFont(font,col);
+                }
                 break;
             case MWX_SET_PEN: // [O]
+                ctx->SetPen(*OBJPEN);
                 break;
             case MWX_SET_BRUSH: // [O]
-                //  Context transforms
+                ctx->SetBrush(*OBJBRUSH);
                 break;
             case MWX_BITMAP: // [O X Y W H]
+                {
+                    const wxBitmap& bmp = *OBJBITMAP;
+                    double x = VTX;
+                    double y = VTX;
+                    double w = VTX;
+                    double h = VTX;
+                    ctx->DrawBitmap(bmp,x,y,w,h);
+                }
                 break;
+            //  Context transforms
             case MWX_ROTATE: // [Radian]
+                ctx->Rotate(VTX);
                 break;
             case MWX_SCALE: // [X Y]
+                {
+                    double x = VTX;
+                    double y = VTX;
+                    ctx->Scale(x,y);
+                }
                 break;
             case MWX_IDENTITY:
+                {
+                    wxGraphicsMatrix mtx = ctx->GetTransform();
+                    mtx.Set(); // Make identity mtx
+                    ctx->SetTransform(mtx);
+                }
                 break;
                 //  Text and query
             case MWX_TEXT_DRAW: // [O X Y]
+                {
+                    const char* text = OBJTEXT;
+                    double x = VTX;
+                    double y = VTX;
+                    ctx->DrawText(wxString::FromUTF8(text),x,y);
+                }
                 break;
             case MWX_TEXT_QUERY_EXTENT: // [O] => [W H Descent externalLeading]
+                {
+                    const char* text = OBJTEXT;
+                    double w,h,dec,ex;
+                    ctx->GetTextExtent(wxString::FromUTF8(text),&w,&h,&dec,&ex);
+                    QRET = w;
+                    QRET = h;
+                    QRET = dec;
+                    QRET = ex;
+                }
                 break;
-            case MWX_TEXT_QUERY_CHAR_EXTENTS: // [O Count] => [Count W ...]
+            case MWX_TEXT_QUERY_CHAR_EXTENTS: // [O Count] => [W ...]
+                {
+                    const char* text = OBJTEXT;
+                    unsigned int count = OBJUINT;
+                    wxArrayDouble ad;
+                    ctx->GetPartialTextExtents(wxString::FromUTF8(text),ad);
+                    for(int j = 0;j!=count;j++){
+                        QRET = ad[j];
+                    }
+                }
                 break;
             default: // INVALID
                 return -1;
         }
+    }
+    // Sanity check
+    if(count_query && (query_ptr > count_query)){
+        return -2;
+    }
+    if(count_op && (op_ptr > count_op)){
+        return -3;
+    }
+    if(count_vtx && (vtx_ptr > count_vtx)){
+        return -4;
+    }
+    if(count_obj && (obj_ptr > count_obj)){
+        return -5;
     }
     return 0;
 }
@@ -169,8 +326,10 @@ int /* 0 for success */
 mwx_graphics_kick(wxWindow* wnd,
                   unsigned char* op, int count_op,
                   double* vtx, int count_vtx,
-                  uintptr_t* obj, int count_obj){
-    return kick(me()->CreateContext(wnd),op,count_op,vtx,count_vtx,obj,count_obj);
+                  uintptr_t* obj, int count_obj,
+                  double* query, int count_query){
+    return kick(me()->CreateContext(wnd),op,count_op,
+                vtx,count_vtx,obj,count_obj,query,count_query);
 }
 
 MOSHEXPORT
@@ -234,7 +393,7 @@ mwx_pen_destroy(wxPen* pen){
 
 NMOSH_CONSTANT_BEGIN(mwx_graphics)
     // Context OPs
-    NMOSH_EXPORT_SYMBOL_INT(MWX_SET_FONT) // [O]
+    NMOSH_EXPORT_SYMBOL_INT(MWX_SET_FONT) // [O R G B A]
     NMOSH_EXPORT_SYMBOL_INT(MWX_SET_PEN) // [O]
     NMOSH_EXPORT_SYMBOL_INT(MWX_SET_BRUSH) // [O]
     //  Context transforms
@@ -245,7 +404,7 @@ NMOSH_CONSTANT_BEGIN(mwx_graphics)
     //  Text and query
     NMOSH_EXPORT_SYMBOL_INT(MWX_TEXT_DRAW) // [O X Y]
     NMOSH_EXPORT_SYMBOL_INT(MWX_TEXT_QUERY_EXTENT) // [O] => [W H Descent externalLeading]
-    NMOSH_EXPORT_SYMBOL_INT(MWX_TEXT_QUERY_CHAR_EXTENTS) // [O Count] => [Count W ...]
+    NMOSH_EXPORT_SYMBOL_INT(MWX_TEXT_QUERY_CHAR_EXTENTS) // [O Count] => [W ...]
     // PATH OPs
     NMOSH_EXPORT_SYMBOL_INT(MWX_PATH_STROKE) 
     NMOSH_EXPORT_SYMBOL_INT(MWX_PATH_FILL_ODDEVEN)

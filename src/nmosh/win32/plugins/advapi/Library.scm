800bf1293b56ad4df55d0fb580973aab2fc7bab7
okuoku <mjt@cltn.org>
2013-08-07 00:31:56 +0900

    advapi: Win32 advapi32 plugin

--- src/nmosh/win32/plugins/advapi/Library.scm
+++ src/nmosh/win32/plugins/advapi/Library.scm
@@ -5,5 +5,7 @@
   (plugin: mosh_advapi)
   (libname: mosh_advapi)
   #(ret name args)
-  (int madvapi_elevate)
+  (int madvapi_reg_open (void* void* int void*))
+  (int madvapi_reg_close (void*))
+  (int madvapi_reg_query (void* void* void* void* void*))
   )
